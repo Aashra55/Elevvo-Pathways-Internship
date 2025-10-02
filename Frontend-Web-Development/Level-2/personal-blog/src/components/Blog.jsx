3ef51dec41e1304cb66ab4d139bad1a8541c18cb
Aashra Saleem <160631977+Aashra55@users.noreply.github.com>
2025-10-02 14:44:32 +0500

    updated

--- Frontend-Web-Development/Level-2/personal-blog/src/components/Blog.jsx
+++ Frontend-Web-Development/Level-2/personal-blog/src/components/Blog.jsx
@@ -2,21 +2,6 @@ import { motion } from "framer-motion";
 import BlogCard from "./BlogCard";
 import React from "react";
 
-const containerVariants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1, 
-    },
-  },
-};
-
-const cardVariants = {
-  hidden: { opacity: 0, y: 20 },
-  show: { opacity: 1, y: 0 },
-};
-
 export default React.memo(function Blog({ visiblePosts }) {
   return (
     <div id="blog" className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 h-auto">
